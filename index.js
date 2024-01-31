b008f049c31af6e81c49161dfcc4423967376204
spaceofnova <mcrealms23@gmail.com>
2024-01-30 20:00:36 -0600

    Port fix for Heroku

--- index.js
+++ index.js
@@ -62,6 +62,4 @@ app.get("*", function (req, res) {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.listen(process.env.PORT || 5000)
